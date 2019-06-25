265f28b7f8eba2660ba45586746a4387aa00a403
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 11:21:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,8 @@ def self.first_X_students_in_grade_10(number)
       SELECT *
       FROM students
       WHERE grade = 10
-      GROUP BY(students)
+      ORDER BY name ASC
+      LIMIT 1
     SQL
  
     DB[:conn].execute(sql).map do |row|
