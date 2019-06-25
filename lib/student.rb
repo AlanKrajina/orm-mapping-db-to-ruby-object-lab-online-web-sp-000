61082183c17d021383c10a93c4411eb9d849484b
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 11:34:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ def self.first_X_students_in_grade_10(number)
          sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
+      WHERE grade = number
       LIMIT 1
     SQL
  
