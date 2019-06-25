ac96409755d09993053c2672f86a8da135ab5e84
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 11:24:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,7 @@ def self.first_X_students_in_grade_10(number)
       SELECT *
       FROM students
       WHERE grade = 10
-      ORDER BY name ASC
+      ORDER BY student.id ASC
       LIMIT 1
     SQL
  
