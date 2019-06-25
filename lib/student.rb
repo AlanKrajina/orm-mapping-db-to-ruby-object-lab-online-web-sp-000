eafac6c42beb0c9d3e35828e84820eb37df2778b
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 11:16:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,8 +100,8 @@ def self.first_X_students_in_grade_10(number)
          sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
-      LIMIT 1
+      ORDER BY(net_worth) 
+      DESC LIMIT 1
     SQL
  
     DB[:conn].execute(sql).map do |row|
