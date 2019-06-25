a20f46d5ae699425b4058e655e117bd6b59c6b65
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 11:34:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ def self.first_X_students_in_grade_10(number)
          sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = self.grade
+      WHERE grade = grade
     SQL
  
     DB[:conn].execute(sql).map do |row|
